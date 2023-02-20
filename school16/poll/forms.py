3fa24cdc3a0bef34af0bbcca7ef90ed364e1f4a7
Sh0ebAnsari <88709392+Sh0ebAnsari@users.noreply.github.com>
2023-02-20 17:50:00 +0530

    adding files

--- school16/poll/forms.py
+++ school16/poll/forms.py
@@ -1,41 +1,79 @@
 from django import forms 
 
 
-class register(forms.Form):
-    username=forms.CharField(initial='Shoeb Ansari')
-    email=forms.EmailField(initial="Shoeb@gamil.com",
-    label="User Email Address ",
-    )
-    age=forms.IntegerField(initial=00)
-    password1=forms.CharField(required=True,
-    error_messages={'required':'password is must!!'},
-    widget=forms.PasswordInput,
-    label='Password',
-    )
-    password2=forms.CharField(required=True,
-    widget=forms.PasswordInput,
-    disabled=True,
-    help_text='User Password Comes Hear..')
+# class Register(forms.Form):
+#     user_name=forms.CharField(initial='Shoeb Ansari')
+#     email=forms.EmailField(initial="Shoeb@gamil.com",
+#     label="User Email Address ",
+#     )
+#     age=forms.IntegerField(initial=00)
+#     password1=forms.CharField(required=True,
+#     error_messages={'required':'password is must!!'},
+#     widget=forms.PasswordInput,
+#     label='Password',
+#     )
+#     password2=forms.CharField(required=True,
+#     widget=forms.PasswordInput,
+#     label="Confirm Password",
+#     help_text='User Password Comes Hear..')
 
-    father_name=forms.CharField(error_messages={'required':"Father Name is ,must...  "})
+    # father_name=forms.CharField(error_messages={'required':"Father Name is ,must...  "})
 
-    mother_name=forms.CharField(
-        widget=forms.TextInput(attrs={'class':'mymothername','id':'mymother2'})
-    )
+    # mother_name=forms.CharField(
+    #     widget=forms.TextInput(attrs={'class':'mymothername','id':'mymother2'})
+    # )
 
-    salary=forms.CharField(widget=forms.NumberInput(attrs={'class':'salary'}))
+    # salary=forms.CharField(widget=forms.NumberInput(attrs={'class':'salary'}))
 
-    email_address=forms.CharField(widget=forms.EmailInput())
-    web_address=forms.CharField(widget=forms.URLInput())
+    # email_address=forms.CharField(widget=forms.EmailInput())
+    # web_address=forms.CharField(widget=forms.URLInput())
 
-    adhar_no=forms.CharField(initial=4837463,widget=forms.HiddenInput)
+    # adhar_no=forms.CharField(initial=4837463,widget=forms.HiddenInput)
 
-    dob=forms.CharField(widget=forms.DateTimeInput)
+    # dob=forms.CharField(widget=forms.DateTimeInput)
 
-    biodata=forms.CharField(widget=forms.Textarea(attrs={'row':20,'cols':40,'class':'biodata'}))
+    # biodata=forms.CharField(widget=forms.Textarea(attrs={'row':20,'cols':40,'class':'biodata'}))
 
-    color=forms.BooleanField(widget=forms.CheckboxInput(attrs={'class':'colos'},check_test=None))
+    # color=forms.BooleanField(widget=forms.CheckboxInput(attrs={'class':'colos'},check_test=None))
 
-    place=forms.ChoiceField(choices=[("","--select--"),('I',"India"),('A',"AMrica"),("J",'japan')])
+    # place=forms.ChoiceField(choices=[("","--select--"),('I',"India"),('A',"AMrica"),("J",'japan')])
 
-    country=forms.NullBooleanField()
+    # country=forms.NullBooleanField()
+
+
+class Register(forms.Form):
+    user_name=forms.CharField(required=True,error_messages={'required':'User name is must'})
+    email=forms.EmailField(required=True,error_messages={'requirde':'Email is required'})
+    age=forms.IntegerField(required=False)
+    password1=forms.CharField(required=False,min_length=5,max_length=10,strip=False)
+    password2=forms.CharField(required=False,min_length=5,max_length=10)
+
+
+    # cleening and validating specifide field 
+    # def Cleen_user_name(self):
+    #     valname=self.cleened_data.get("user_name")
+    #     if len(valname)<5:
+    #         raise forms.ValidationError("Enter user name length greater then or equal to 5 ")
+    #     return valname
+
+    # def Cleen_Password1(self):
+    #     pass1=self.cleened_data.get("Password1")
+    #     if len(pass1)<5:
+    #         raise forms.ValidationError("Enter password length greater then or equal to 8 ")
+    #     return pass1    
+        
+    # def Cleen_Password2(self):
+    #     pass2=self.cleened_data.get("Password2")
+    #     if len(pass2)<5:
+    #         raise forms.ValidationError("Enter password conform length greater then or equal to 8 ")
+    #     return pass2
+
+
+    # cleening and validating at onese
+    def cleen(self):
+        super(Register,self).cleen()
+        username=self.cleaned_data.get("user_name")
+        pass1=self.cleaned_data.get("password1")
+        pass2=self.cleaned_data.get("password2")
+        if len(user)
+
--- school16/poll/views.py
+++ school16/poll/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .forms import register
+from django.shortcuts import render,HttpResponse
+from .forms import Register
 # Create your views here.
 
 def home(request):
@@ -7,13 +7,58 @@ def home(request):
     # fm=register(auto_id=True)
     # fm=register(auto_id=False)
     # fm=register(auto_id="id_%s",label_suffix=" ")
-    fm=register(
-        auto_id="id_%s",
-        label_suffix="-",
-        initial={
-            'username':'ankit kumar',
-            'email':'abc1234@gmail.com',
-    }
-    )
+    # fm=register(
+    #     auto_id="id_%s",
+    #     label_suffix="-",
+    #     initial={
+    #         'username':'ankit kumar',
+    #         'email':'abc1234@gmail.com',
+    # }
+    # )
     # fm.order_fields(['email','username','password2','password1','age'])
+    # if request.method=='POST':
+    #     fm=Register(request.POST)
+    #     print("validation : ",fm.is_valid())
+    #     print("Clening The Data".center(40,"*"))
+    #     # print("User Name :" ,fm.cleaned_data['user_name'])
+    #     # print("Email address :",fm.cleaned_data['email'])
+    #     # print("Usre Age :",fm.cleaned_data['age'])
+    #     # print("Password1 :",fm.cleaned_data['password1'])
+    #     # print("Password2 :",fm.cleaned_data['password2'])
+    #     # Or 
+    #     print("User Name :" ,request.POST['user_name'])
+    #     print("Email address :",request.POST['email'])
+    #     print("Usre Age :",request.POST['age'])
+    #     print("Password1 :",request.POST['password1'])
+    #     print("Password2 :",request.POST['password2'])
+    # else:
+    #     fm=Register()
+    
+    # return render(request,'home.html',{'form':fm,'title':title})
+
+
+
+
+    if request.method=='POST':
+        fm=Register(request.POST)
+        print("validation : ",fm.is_valid())
+        print("Clening The Data".center(40,"*"))
+        # print("User Name :" ,fm.cleaned_data['user_name'])
+        # print("Email address :",fm.cleaned_data['email'])
+        # print("Usre Age :",fm.cleaned_data['age'])
+        # print("Password1 :",fm.cleaned_data['password1'])
+        # print("Password2 :",fm.cleaned_data['password2'])
+        # Or 
+        print("User Name :" ,request.POST['user_name'])
+        print("Email address :",request.POST['email'])
+        print("Usre Age :",request.POST['age'])
+        print("Password1 :",request.POST['password1'])
+        print("Password2 :",request.POST['password2'])
+        # return HttpResponse("Your account is created sucessfully!!{}")
+        return render(request,'thanks.html')
+
+    else:
+
+        fm=Register()
+    
     return render(request,'home.html',{'form':fm,'title':title})
