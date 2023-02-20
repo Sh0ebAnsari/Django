4dad0433d661ee0782887a5e1b29347c2b1dfeb5
Sh0ebAnsari <88709392+Sh0ebAnsari@users.noreply.github.com>
2023-02-20 17:35:16 +0530

    adding file

--- school16/poll/forms.py
+++ school16/poll/forms.py
@@ -16,3 +16,26 @@ class register(forms.Form):
     widget=forms.PasswordInput,
     disabled=True,
     help_text='User Password Comes Hear..')
+
+    father_name=forms.CharField(error_messages={'required':"Father Name is ,must...  "})
+
+    mother_name=forms.CharField(
+        widget=forms.TextInput(attrs={'class':'mymothername','id':'mymother2'})
+    )
+
+    salary=forms.CharField(widget=forms.NumberInput(attrs={'class':'salary'}))
+
+    email_address=forms.CharField(widget=forms.EmailInput())
+    web_address=forms.CharField(widget=forms.URLInput())
+
+    adhar_no=forms.CharField(initial=4837463,widget=forms.HiddenInput)
+
+    dob=forms.CharField(widget=forms.DateTimeInput)
+
+    biodata=forms.CharField(widget=forms.Textarea(attrs={'row':20,'cols':40,'class':'biodata'}))
+
+    color=forms.BooleanField(widget=forms.CheckboxInput(attrs={'class':'colos'},check_test=None))
+
+    place=forms.ChoiceField(choices=[("","--select--"),('I',"India"),('A',"AMrica"),("J",'japan')])
+
+    country=forms.NullBooleanField()
